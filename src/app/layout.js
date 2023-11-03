efe24fbac0e06b2fadd4bda4a93d61fc806579e8
wjbetech <wjbetech@gmail.com>
2023-11-04 01:31:08 +0900

    Finished 'AddBook' page with routing plus basic styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
+import NavBar from "./(components)/NavBar";
+import SideBar from "./(components)/SideBar";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar";
-import Sidebar from "./components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
-				<NavBar />
-				<Sidebar />
-				{children}
+			<body
+				className={`${inter.className} flex-grow`}
+			>
+				<div className="flex flex-col h-screen max-h-screen">
+					<NavBar />
+					<div className="flex">
+						<SideBar />
+						<div className="w-full">
+							{children}
+						</div>
+					</div>
+				</div>
 			</body>
 		</html>
 	);
